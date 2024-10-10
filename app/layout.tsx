e9c7ee9096baef956b7a83b5fddad47dd3a4a26d
ClaraChrstyln <Clarachristylin3007@gmail.com>
2024-10-10 22:42:45 +0700

    coba

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,22 @@
 import '@/app/ui/global.css';
- 
-export default function RootLayout({
-  children,
-}: {
+import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export default function RootLayout({children,}: {
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
